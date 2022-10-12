a17b0cb74b842a62e2f2328a344d0f843e62e13d
alex-lopes-databricks <alex-lopes-databricks>
2022-10-12 01:21:49 +0000

    first commit

---
+++ Bash.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+# MAGIC %sh
+# MAGIC echo "Starting on bash"
+# MAGIC my_function () {
+# MAGIC   print "My function was called";
+# MAGIC }
+# MAGIC my_function
+
+# COMMAND ----------
+
+# MAGIC %sh 
+# MAGIC my_function
+
+# COMMAND ----------
+
+
